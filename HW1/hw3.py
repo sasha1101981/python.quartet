502c2528c542497abcdedfecfaceb463a537771b
sasha1101981 <124509399+sasha1101981@users.noreply.github.com>
2024-04-16 16:44:04 +0700

    first commit3

--- HW1/hw3.py
+++ HW1/hw3.py
@@ -7,19 +7,21 @@ from random import randint
  
 LOWER_LIMIT = 0
 UPPER_LIMIT = 1000
-num = randint(LOWER_LIMIT, UPPER_LIMIT)
-count = 1
-print(f'Угадай целое число от 1 до 1000. У тебя 10 попыток.\n')
+n = randint(LOWER_LIMIT, UPPER_LIMIT)
+attempt = 0
 
-while count <= 10:
-    n = int(input(f'Попытка {count}, введи число: '))
-    if n < num:
-        print('Загаданное число больше. Попробуй ещё раз.')
-        count += 1
-    elif n > num:
-        print('Загаданное число меньше. Попробуй ещё раз.')
-        count += 1
-    else:
-        print(f'Молодец! Ты угадал число {num} за {count} попыток!')
-    print(f'У тебя осталось {11 - count} попыток.\n')
-print(f'К сожалению, тебе не удалось угадать число {num} за 10 попыток')
+while True:
+    print("Я загадал число от 1 до 1000, угадай его :)")
+    user_num = int(input("Ваша догадка: "))
+    attempt += 1
+    if user_num == n:
+        print(f"Ты угадал число, молодец!\nКоличество твоих попыток: {attempt}\nСпасибо за игру!")
+        break
+    elif user_num > n:
+        print("Моё число меньше.")
+    elif user_num < n:
+        print("Моё число больше")
+
+
+
+    
